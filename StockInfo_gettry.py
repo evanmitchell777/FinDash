5912c943b698d13728fb91cce4819fae840a5b36
Evan Mitchell <43766252+evanmitchell777@users.noreply.github.com>
2023-05-18 15:47:01 -0500

    Update StockInfo_gettry.py

--- StockInfo_gettry.py
+++ StockInfo_gettry.py
@@ -1,29 +1,27 @@
+#To do: Make it to where data is stored somewhere in memory (text file csv etc.) 
 import yfinance as yf
-import csv
 
-def process_data(ticker):
-    # Download stock data from yfinance
-    stock = yf.Ticker(ticker)
-    history = stock.history(period="max")
+fund_name = input("Enter the ticker symbol of the mutual fund: ")
 
-    # Calculate the response column
-    rows = []
-    for i in range(len(history)-1):
-        today_close = history.iloc[i]['Close']
-        tomorrow_close = history.iloc[i+1]['Close']
-        if tomorrow_close > today_close:
-            response = 'buy'
-        else:
-            response = 'sell'
-        rows.append([history.index[i].strftime("%Y-%m-%d"), today_close, history.iloc[i]['Open'], history.iloc[i]['High'], history.iloc[i]['Low'], response])
-    rows.append([history.index[-1].strftime("%Y-%m-%d"), history.iloc[-1]['Close'], history.iloc[-1]['Open'], history.iloc[-1]['High'], history.iloc[-1]['Low'], ''])
+# Create a Ticker object
+fund = yf.Ticker(fund_name)
 
-    # Write the data to a csv file
-    with open(f'{ticker}.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(['Date', 'Close', 'Open', 'High', 'Low', 'Response'])
-        writer.writerows(rows)
+# Gathers YTD performance
+YTD = fund.history(period="YTD")['Close'].pct_change()[-1] * 100
 
-if __name__ == '__main__':
-    ticker = input('Enter stock ticker symbol: ')
-    process_data(ticker)
+# Gathers one-year performance
+one_year_performance = fund.history(period="1y")['Close'].pct_change()[-1] * 100
+
+# Gathers three-year performance
+three_year_performance = fund.history(period="3y")['Close'].pct_change()[-1] * 100
+
+# Gathers five-year performance
+five_year_performance = fund.history(period="5y")['Close'].pct_change()[-1] * 100
+
+# Gathers ten-year performance
+ten_year_performance = fund.history(period="10y")['Close'].pct_change()[-1] * 100
+
+# Get the dividend info
+dividends = fund.dividends
+latest_dividend_value = dividends[-1]
+print(latest_dividend_value)
